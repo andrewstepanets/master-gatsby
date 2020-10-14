bc5f7a85f2a29f1d5fb5eb63ea598bbebbd1cbc8
Andrii Stepanets <ovrass@gmail.com>
2020-10-15 01:01:43 +0300

    Added styles for Nav and Logo

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -5,11 +5,13 @@ import Footer from './Footer';
 // normalize css
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children }) {
   return (
     <div>
       <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
