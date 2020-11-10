967875143dc3eacf6e73524d9feabd86042fa643
Andrii Stepanets <ovrass@gmail.com>
2020-11-10 20:22:21 +0200

    Started order page

---
+++ gatsby/src/utils/useForm.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export default function useForm(defaults) {
+  const [values, setValues] = useState(defaults);
+
+  function updateValue(e) {
+    // check if its a number and convert
+    let { value } = e.target;
+    if (e.target.type === 'number') {
+      value = parseInt(e.target.value);
+    }
+    setValues({
+      // copy the existing value into it
+      ...values,
+      // update the new value that change
+      [e.target.name]: e.target.value,
+    });
+  }
+  return { values, updateValue };
+}
