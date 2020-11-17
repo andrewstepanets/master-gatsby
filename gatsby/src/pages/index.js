9406c4deddffa40fcb7024b72c7c97fd5e676780
Andrii Stepanets <ovrass@gmail.com>
2020-11-17 19:44:01 +0200

    Created a Store settings page in the Sanity

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing() {
+  return (
+    <div>
+      <p>Currently Slicing</p>
+    </div>
+  );
+}
+function HotSlices() {
+  return (
+    <div>
+      <p>Hot Slices</p>
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { sliceMasters, hotSlices } = useLatestData();
+  console.log(sliceMasters);
   return (
-    <>
-      <p>Hey! I'm the home page</p>
-      <p>Hey! I'm another element</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza Downtown</h1>
+      <p>Open 9:00 to 22:00 Every Single Day</p>
+      <div>
+        <CurrentlySlicing sliceMasters={sliceMasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
   );
 }
